d5d8d9b9e455b20ace8b9be402dcbdc34943eb30
brightw <wugm1977@yahoo.com.cn>
2017-11-16 21:46:17 +0800

    change code

--- list_movies.py
+++ list_movies.py
@@ -1,1 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
 import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+main_url = 'http://www.dy2018.com'
+resp = requests.get(main_url)
+resp.encoding = 'gb18030'
+
+print(resp.text)
+soup = BeautifulSoup(resp.text, 'lxml')
+##for child in soup.body.children:
+##    print(child)
+
+for li in soup.select('li'):
+    if li.a is not None:
+        li_string = li.a.string
+        print("li.a attr", li.a.attrs['title'])
+        print(li_string)
