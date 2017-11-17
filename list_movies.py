a894675878bd8bde2d4deeb5358df54f650389b1
brightw <15052439@qq.com>
2017-11-17 16:50:30 +0800

    code change
    code change

--- list_movies.py
+++ list_movies.py
@@ -6,35 +6,52 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-g_main_url = 'http://www.dy2018.com'
+dytd_main_url = 'http://www.dy2018.com'
+ygdy_mail_url = 'http://www.ygdy8.com'
+
+def is_correct_href(href):
+    return href is not None and href.get('href') is not None
 
 
-def is_correct_li(li):
-    return li.a is not None and li.a.get('href') is not None
-
-def is_high_li(li):
-    if is_correct_li(li) == False:
+def is_high_href(href):
+    if is_correct_href(href) == False:
         return False
     
-    match = re.search(r'(20\d[789]).*', li.a.string)
-    match = match and re.search(r'.*([789]\.\d).*', li.a.string)
+    match = re.search(r'(20\d[789]).*', href.string)
+    match = match and re.search(r'.*([789]\.\d).*', href.string)
     score = float(match.group(1)) if match else 0.0
-    return score >= 7.0
 
-def print_li(li):
-    print("{}  <{}>".format(li.a.string, g_main_url+li.a.get('href')))
+    return score >= 7.0 or href.string.find(u'\u9ad8\u5206') != -1
+
+def print_href(href, main_url):
+    print("{:>60}  <{}>".format(href.string, main_url+href.get('href')))
+
+
+def list_dytd():
+    resp = requests.get(dytd_main_url)
+    resp.encoding = 'gb18030'
+    soup = BeautifulSoup(resp.text, 'lxml')
+    for href in soup.select('div .co_content222 ul li a'):
+        if is_high_href(href):
+            print_href(href, dytd_main_url)
+
+def list_ygdy():
+    resp = requests.get(ygdy_mail_url)
+    resp.encoding = 'gb18030'
+    soup = BeautifulSoup(resp.text, 'lxml')
+    for href in soup.select('div .co_content8 td a'):
+        if is_high_href(href):
+            print_href(href, ygdy_mail_url)
+
+
 
 def main():
 
-    resp = requests.get(g_main_url)
-    resp.encoding = 'gb18030'
-
-    #print(resp.text)
-    soup = BeautifulSoup(resp.text, 'lxml')
-
-    for li in soup.select('div .co_content222 ul li'):
-        if is_high_li(li):
-            print_li(li)
+    print("************************************  {}  ************************************".format(dytd_main_url))
+    list_dytd()
+    
+    print("************************************  {}  ************************************".format(ygdy_mail_url))
+    list_ygdy()
 
 if __name__ == '__main__':
     main()
